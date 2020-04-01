f49dc76d19c0e2b6cc488a68a5badd5bb80b5d4b
yangzhi <hzyangzhi@highstore.cn>
2020-04-01 15:39:22 +0800

    初始化

---
+++ src/main/java/com/example/httpClientDemo/HttpClientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.httpClientDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpClientDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpClientDemoApplication.class, args);
+    }
+
+}
